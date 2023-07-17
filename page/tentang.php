ac712784a0d22335c273032f057a8fafbfae9da0
Muhammad alfiansyah <114557064+MuhAlfiansyah@users.noreply.github.com>
2023-07-17 18:50:41 +0700

    Add files via upload

---
+++ page/tentang.php
@@ -0,0 +1,58 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <img src="img/hp_terbaru.png" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: User </td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>NIM</b></li>
+        </td>
+        <td>: 12345678</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: </td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Hobby</b></li>
+        </td>
+        <td>: </td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Minat</b></li>
+        </td>
+        <td>: I'M Web Developer, PHP addict, Full Stack Developer, Laravel</td>
+      </ul>
+    </tr>
+
+   
+
+  </table>
+
+</div>
